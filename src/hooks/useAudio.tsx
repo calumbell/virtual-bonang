8d9a8a638ea6689b2f5e87e58346741371eae0a5
calumbell <calumabell@googlemail.com>
2023-09-11 09:11:15 +0100

    added short fade-in on audio to reduce clicks

--- src/hooks/useAudio.tsx
+++ src/hooks/useAudio.tsx
@@ -7,7 +7,16 @@ export default function useAudio(src: string) {
   if (!audio) return () => {};
 
   return () => {
-    audio.currentTime = 0;
-    audio.play();
+    // To reduce clicking on retriggers, first fade out previous sound
+    const decrementVolume = () => (audio.volume /= 16);
+    const interval = setInterval(decrementVolume, 1);
+
+    // Wait ~10 ms for audio to fade out before restarting
+    setTimeout(() => {
+      clearInterval(interval);
+      audio.volume = 1;
+      audio.currentTime = 0;
+      audio.play();
+    }, 10);
   };
 }
