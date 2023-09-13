83a73ff6d05d56a57e4d66a4ae76e734888996c5
calumbell <calumabell@googlemail.com>
2023-09-13 13:11:14 +0100

    compressed slendro bonang audio

--- public/data/slendroBonang.tsx
+++ public/data/slendroBonang.tsx
@@ -2,54 +2,54 @@ const slendroBonang = [
   [
     {
       note: "6",
-      src: "/audio/slendro/bnsl-6h.wav",
+      src: "/audio/slendro/bnsl-6h.mp3",
       keybind: "q",
     },
     {
       note: "5",
-      src: "/audio/slendro/bnsl-5h.wav",
+      src: "/audio/slendro/bnsl-5h.mp3",
       keybind: "w",
     },
     {
       note: "3",
-      src: "/audio/slendro/bnsl-3h.wav",
+      src: "/audio/slendro/bnsl-3h.mp3",
       keybind: "e",
     },
     {
       note: "2",
-      src: "/audio/slendro/bnsl-2h.wav",
+      src: "/audio/slendro/bnsl-2h.mp3",
       keybind: "r",
     },
     {
       note: "!",
-      src: "/audio/slendro/bnsl-1h.wav",
+      src: "/audio/slendro/bnsl-1h.mp3",
       keybind: "t",
     },
   ],
   [
     {
       note: "1",
-      src: "/audio/slendro/bnsl-1.wav",
+      src: "/audio/slendro/bnsl-1.mp3",
       keybind: "a",
     },
     {
       note: "w",
-      src: "/audio/slendro/bnsl-2.wav",
+      src: "/audio/slendro/bnsl-2.mp3",
       keybind: "s",
     },
     {
       note: "e",
-      src: "/audio/slendro/bnsl-3.wav",
+      src: "/audio/slendro/bnsl-3.mp3",
       keybind: "d",
     },
     {
       note: "t",
-      src: "/audio/slendro/bnsl-5.wav",
+      src: "/audio/slendro/bnsl-5.mp3",
       keybind: "f",
     },
     {
       note: "y",
-      src: "/audio/slendro/bnsl-6.wav",
+      src: "/audio/slendro/bnsl-6.mp3",
       keybind: "g",
     },
   ],
