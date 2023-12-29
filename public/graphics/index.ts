a26127b54650f0d2232fb729f99cad8a57a0c25d
calumbell <calumabell@googlemail.com>
2023-12-29 11:55:11 +0000

    added information icon svg

--- public/graphics/index.ts
+++ public/graphics/index.ts
@@ -1,4 +1,6 @@
 import bonangSvgData from "./bonang-svg-data.json";
-import keyboardSvgData from "./keyboard-svg-data.json";
 import githubLogoSvgData from "./github-logo-svg-data.json";
-export { bonangSvgData, githubLogoSvgData, keyboardSvgData };
+import infoIconSvgData from "./information-svg-data.json";
+import keyboardSvgData from "./keyboard-svg-data.json";
+
+export { bonangSvgData, githubLogoSvgData, infoIconSvgData, keyboardSvgData };
