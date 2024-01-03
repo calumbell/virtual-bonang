ec4888ad5d43efc2748356c4ffa2a82b8ff72065
calumbell <calumabell@googlemail.com>
2024-01-03 22:05:01 +0000

    updated Seo cmpnt, added opengraph-image

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -3,8 +3,12 @@ import Head from "next/head";
 export default function Seo({
   title = "Virtual Bonang",
   description = "Pracitce playing the Bonang instrument from the Javanese Gamelan online.",
-  keywords = "Bonang, Gamelan, Javanese Gamelan, Bonang Barung, Virtual Bonang, Virtual Gamelan, Gamelan Simulator",
+  keywords = "Bonang, Gamelan, Javanese Gamelan, Bonang Barung, Virtual Bonang, Virtual Gamelan, Gamelan Simulator, Digital Gamelan, Gamelan Online",
+  url = process.env.NODE_ENV == "production"
+    ? "" /* TODO: Production URL tbd */
+    : "http://localhost:3000",
 }) {
+  console.log(`${url}/opengraph_image.svg`);
   return (
     <Head>
       <title>{title}</title>
@@ -14,7 +18,15 @@ export default function Seo({
       <meta property="keywords" content={keywords} />
       <meta property="author" content="Calum Bell" />
       <meta property="og:title" content={title} />
+      <meta property="og:type" content="website" />
       <meta property="og:description" content={description} />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={`${url}/opengraph-image.svg`} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@bluesky" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={`${url}/opengraph-image.svg`} />
     </Head>
   );
 }
