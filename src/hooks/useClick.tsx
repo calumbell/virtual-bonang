5fdecd139ac07e3039c9aacd0ce90a71d576379e
calumbell <calumabell@googlemail.com>
2024-01-16 20:42:59 +0000

    useClick hook now fires on spacebar when focused

--- src/hooks/useClick.tsx
+++ src/hooks/useClick.tsx
@@ -8,16 +8,26 @@ export default function useClick(
   useEffect(() => {
     if (!ref.current) return;
     const element = ref.current;
-    const onClick = (e: Event) => {
+
+    // Event handler for executing callback function
+    const onClick = (e: Event | KeyboardEvent) => {
       e.preventDefault();
       callback();
     };
 
-    // register event listener & cleanup
+    // Triggers a click when element is focused & spacebar is pressed
+    const onFocusClick = (e: KeyboardEvent) => {
+      if (document.activeElement !== ref.current) return;
+      if (e.code !== "Space") return;
+      onClick(e as KeyboardEvent);
+    };
 
+    // register event listener & cleanup
     element.addEventListener("mousedown", onClick);
+    element.addEventListener("keydown", onFocusClick);
     return () => {
       element.removeEventListener("mousedown", onClick);
+      element.removeEventListener("keydown", onFocusClick);
     };
   }, [ref, callback]);
 }
